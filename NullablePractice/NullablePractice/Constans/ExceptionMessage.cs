5322275ca6388ddb7b99aaf9af6095f4e7c520e4
Javid A. Ismayilzada <javidai@code.local>
2023-01-09 18:51:13 +0400

    New

---
+++ NullablePractice/NullablePractice/Constans/ExceptionMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullablePractice.Constans
+{
+    public class ExceptionMessage
+    {
+        public const string InvalidLogin = "Username or password wrong";
+    }
+}
---
+++ NullablePractice/NullablePractice/Exception/InvalidLoginException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullablePractice.Exceptions
+{
+    public class InvalidLoginException : Exception
+    {
+        public InvalidLoginException(string message) : base(message) { }
+    }
+}
